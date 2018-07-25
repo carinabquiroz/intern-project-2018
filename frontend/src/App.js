81aca724f874e12f0e846f52d4596bac82ab38c0
Frances Hartwell <macbook@macbook.local>
2018-07-25 17:05:17 -0400

    hooked up basic username/password entry to express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+import Signup from './components/signup/signup';
 
-class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
-}
+// Use JSX instead of classes (when possible):
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <Signup />
+  </div>
+);
 
 export default App;
