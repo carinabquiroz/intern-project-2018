9809c27ae1be94da53c92537b9a67922ea7876e0
Frances Hartwell <macbook@macbook.local>
2018-07-26 12:48:52 -0400

    beginning to add route functionality to main app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import styled from 'styled-components';
+
 import logo from './logo.svg';
 import './App.css';
-import Signup from './components/signup/signup';
+import Signup from './components/signup/';
+import Home from './components/home/';
+import BasicExample from './components/examples/router';
 
-// Use JSX instead of classes (when possible):
+const StyledDiv = styled.div`
+  text-align: center;
+`;
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    <Signup />
-  </div>
+  <Router>
+    <StyledDiv>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Welcome to React</h1>
+      </header>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/signup'>Signup</Link></li>
+      </ul>
+      <hr />
+      <Route exact path='/' component={Home}/>
+      <Route path='/signup' component ={Signup} />
+    </StyledDiv>
+  </Router>
 );
 
 export default App;
