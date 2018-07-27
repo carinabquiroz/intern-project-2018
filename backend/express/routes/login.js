81337de58b22820345e3d9aea319f282f4929aeb
frances-h <franceshartwell09@gmail.com>
2018-07-27 15:58:48 -0400

    add json web tokens to API (#43)

--- backend/express/routes/login.js
+++ backend/express/routes/login.js
@@ -1,24 +1,26 @@
 import db from '../db';
 import bcrypt from 'bcrypt';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 var router = express.Router();
 
 router.post('/', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  db.user.findAll({ where: { username: username } })
-    .then((data) => {
-      if (data.length == 0) {
-        console.log('data length was 0');
-        res.write(JSON.stringify({ loggedIn: false }));
-        res.end();
-      } else {
-        console.log(data);
-        res.write(JSON.stringify({ loggedIn: bcrypt.compareSync(password, data[0].hash) }));
-        res.end();
-      };
+  db.user.findOne({ where: { username: username } })
+  .then(user => {
+    if (!user) return res.status(401).send({ auth: false, token: null });
+    const passwordIsValid = bcrypt.compareSync(password, user.hash);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    const token = jwt.sign({ id: user.id }, process.env.FAKE_SECRET, {
+      expiresIn: 86400,
     });
+    res.status(200).send({ auth: true, token: token });
+  })
+  .catch(err => {
+    res.status(500).send('Error on the server');
+  });
 });
 
 module.exports = router;
