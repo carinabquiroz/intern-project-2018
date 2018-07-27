82c7b496879707e4c9182e88c7722956ef032a79
Frances Hartwell <macbook@macbook.local>
2018-07-27 15:50:57 -0400

    add json web tokens to API

--- backend/express/routes/register.js
+++ backend/express/routes/register.js
@@ -1,11 +1,26 @@
-import storeUser from '../utils/storeUser';
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+require('dotenv').config();
+
+import db from '../db';
+
 var router = express.Router();
 
 router.post('/', (req, res) => {
-  console.log('Adding user to db');
-  storeUser(req.body);
-  res.end();
+  const saltRounds = 10;
+  const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
+
+  db.user.create({
+    username: req.body.username,
+    hash: hashedPassword,
+  }).then(user => {
+      var token = jwt.sign({ id: user._id }, process.env.FAKE_SECRET, {
+        expiresIn: 86400,
+      });
+      res.status(200).send({ auth: true, token: token });
+    }
+  ).catch(err => res.status(500).send('There was a problem registering the user'));
 });
 
 module.exports = router;
--- backend/express/routes/users.js
+++ backend/express/routes/users.js
@@ -1,4 +1,3 @@
-import storeUser from '../utils/storeUser';
 import express from 'express';
 var router = express.Router();
 
@@ -14,12 +13,6 @@ router.get('/', (req, res, next) => {
     username: 'D0loresH4ze',
   },
 ]);
-});
-
-router.post('/', (req, res) => {
-  console.log('Adding user to db');
-  storeUser(req.body);
-  res.end();
 });
 
 module.exports = router;
