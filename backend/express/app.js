03290d34bfd626b6b25cd3275d736848800386ae
Carina Quiroz <cquiroz@xogrp.com>
2018-07-27 15:32:19 -0400

    done

--- backend/express/app.js
+++ backend/express/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register');
 var loginRouter = require('./routes/login');
+var createEventRouter = require('./routes/createEvent');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/createEvent', createEventRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
