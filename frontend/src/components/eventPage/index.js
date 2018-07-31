d1dbd2dc9d664631f5148b0c62ab1208f34ed87c
carinabquiroz <35759507+carinabquiroz@users.noreply.github.com>
2018-07-31 15:40:37 -0400

    you can list events (#48)
    * you can list events
    
    * fixed

---
+++ frontend/src/components/eventPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class EventPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}</h3>
+        <h3>{this.props.info.description}</h3>
+        <h3>{this.props.info.date}</h3>
+        <h3>{this.props.info.time}</h3>
+        <h3>{this.props.info.location}</h3>
+      </div>
+    );
+  }
+}
+
+export default EventPage;
