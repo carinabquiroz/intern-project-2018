256ebafc9fd952c018aab7c74b85857e45e3f4d1
Carina Quiroz <cquiroz@xogrp.com>
2018-08-02 17:17:08 -0400

    backend done

--- backend/express/models/user.js
+++ backend/express/models/user.js
@@ -4,4 +4,5 @@ export default {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   username: { type: Sequelize.STRING, allowNull: false },
   hash: { type: Sequelize.STRING, allowNull: false},
+  events: { type: Sequelize.ARRAY(Sequelize.INTEGER), allowNull: false}
 };
