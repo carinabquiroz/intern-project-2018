bc41e47ff1a7cb0894438bd36d0955632f6de9e8
carinabquiroz <35759507+carinabquiroz@users.noreply.github.com>
2018-08-13 13:02:42 -0400

    Cq/fix register (#98)
    * better registration instructions

--- frontend/src/utils/username.js
+++ frontend/src/utils/username.js
@@ -1,5 +1,5 @@
 const isGoodUsername = username => {
-  const goodUsernameRegex = RegExp("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
+  const goodUsernameRegex = RegExp("^(?=.{4,15}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
   return goodUsernameRegex.test(username);
 }
 
