4c01e21db46d7b4d7a94e70d1514a0e640debba2
Carina Quiroz <cquiroz@xogrp.com>
2018-08-13 11:54:02 -0400

    nav bar and title stylized

--- frontend/src/components/navbar/navlink.js
+++ frontend/src/components/navbar/navlink.js
@@ -3,12 +3,28 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavDiv = styled.div`
-  padding: 0 30px;
+  padding: 15px 35px;
+  :hover {
+    background: #BB785F
+  }
+  :active {
+    background: #925d4A
+  }
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: #FFFFFF
+  display: inline-block;
+  position: relative;
+  z-index: 1;
+  padding: 1em 4em;
+  margin: -2em;
 `;
 
 const Navlink = props =>
   <NavDiv>
-    <Link to={props.to}>{props.label}</Link>
+    <NavLink to={props.to}>{props.label}</NavLink>
   </NavDiv>;
 
 export default Navlink;
